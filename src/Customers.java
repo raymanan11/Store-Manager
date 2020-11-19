dff2872447a3e5984aab5d77842e3cf686d2c488
banh <60904232+banhmic@users.noreply.github.com>
2020-11-18 16:44:32 -0800

    Customer class
    added get and set methods

--- src/Customers.java
+++ src/Customers.java
@@ -1,3 +1,105 @@
-public class Customers {
+import java.util
+import java.util.Scanner;*;
 
+public class Customers {
+	
+	private String lastName;
+	private String firstName;
+	private Date DoB;
+	private String address;
+	private String phoneNum;
+	private String email;
+	private String paymentInfo;
+	
+	public Customer() {
+		
+		lastName = "";
+		firstName = "";
+		DoB = new Date();
+		address = "";
+		phoneNum = "";
+		email = "";
+		paymentInfo = "";
+	}
+	
+	public Customer(String lName, String fName, Date birthday, String homeAddress, String phoneNumber, 
+						String emailAddress, String payment, ) {
+		
+		lastName = lName;
+		firstName = fName;
+		DoB = birthday;
+		address = homeAddress;
+		phoneNum = phoneNumber;
+		email = emailAdress;
+		paymentInfo = payment;
+	}
+	
+	private String getCustomerName() {
+		
+		String name = firstName + " " + lastName;
+		return name;
+	}
+	
+	private String getCustomerAddress() {
+		
+		return address;
+	}
+	
+	private String getCustomerPhoneNumber() {
+		
+		return phoneNum;
+	}
+	
+	private String getCustomerEmail() {
+		
+		return email;
+	}
+	
+	private String getCustomerPaymentInfo() {
+		
+		return paymentInfo;
+	}
+	
+	private Date getDoB() {
+		
+		return birthday;
+	}
+	
+	private getCustomer() {
+		
+		return this;
+	}
+	
+	private setCustomer() {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter last name: ");
+		String lname = in.next();
+		
+		System.out.print("Enter first name: ");
+		String fname = in.next();
+		
+		System.out.print("Enter date of birth: ");
+		System.out.print("Year: ");
+		String y = in.nextInt();
+		System.out.print("Month: ");
+		String m = in.nextInt();
+		System.out.print("Day: ");
+		String d = in.nextInt();
+		Date birthday = new Date(y, m , d)
+		
+		System.out.print("Enter shipping/home address: ");
+		String homeAddress = in.next();
+		
+		System.out.print("Enter phone number: ");
+		String phoneNumber = in.next();
+		
+		System.out.print("Enter email address: ");
+		String emailAddress = in.next();
+		
+		System.out.print("Enter payment information: ");
+		String payment = in.next();
+		
+		Customer c = new Customer(lname, fname, birthday, homeAddress, phoneNumber, emailAddress, payment);
+	}
 }
--- src/Products.java
+++ src/Products.java
@@ -1,3 +1,33 @@
 public class Products {
-
+	
+	private int productNum;
+	private String productName;
+	private int quantity;
+	private double productPrice;
+	private double productRetail;
+	private int amtSold;
+	
+	public Product() {
+		
+		productNum = 0;
+		productName = "";
+		quantity = 0;
+		productPrice = 0.0;
+		productRetail = 0.0;
+		amtSold = 0;
+	}
+	
+	public Product(int num, String name, int stock, double price
+					double retail, int sale) {
+		
+		productNum = num;
+		productName = name;
+		quantity = stock;
+		productPrice = price;
+		productRetail = retail;
+		amtSold = sale;
+	}
+	
+	
+	
 }
