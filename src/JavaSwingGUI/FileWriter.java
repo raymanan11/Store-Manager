12e8331f07ca6eba54a6244abeda740ae14d05c9
Raymond An <raymondan100@gmail.com>
2020-12-03 17:17:20 -0800

    Have both methods of adding in products
    Method that orders products in decreasing profit % is bugged if duplicate profit percent because map can't hold duplicate values

--- src/JavaSwingGUI/FileWriter.java
+++ src/JavaSwingGUI/FileWriter.java
@@ -23,23 +23,52 @@ public class FileWriter {
         return null;
     }
 
+    // gets profit % in decreasing order but if duplicate profit % then shows the same
+    // row twice because of map not allowing duplicates
+
+//    public ArrayList<ArrayList<String>> readFromFile(String fileName) {
+//        try {
+//            ArrayList<ArrayList<String>> resultSet = new ArrayList<>();
+//            ArrayList<String> productInfo;
+//            Scanner file = new Scanner(new File(fileName));
+//            Gson gson = new Gson();
+//            ArrayList<Double> profitPercent = new ArrayList<>();
+//            Map<Double, ArrayList<String>> orderedProfit = new HashMap<>();
+//            while (file.hasNextLine()) {
+//                productInfo = new ArrayList<>();
+//                Products product = gson.fromJson(file.nextLine(), Products.class);
+//                getProducts(productInfo, product);
+//                System.out.println(product.getProfitPercent());
+//                profitPercent.add(product.getProfitPercent());
+//                orderedProfit.put(product.getProfitPercent(), productInfo);
+//            }
+//            Collections.sort(profitPercent);
+//            System.out.println(profitPercent);
+//            for (int i = 0; i < profitPercent.size(); i++) {
+//                resultSet.add(orderedProfit.get(profitPercent.get(profitPercent.size() - 1 - i)));
+//            }
+//            file.close();
+//            return resultSet;
+//        }
+//        catch (IOException e) {
+//            System.out.println("Wrong file name!");
+//        }
+//        return null;
+//    }
+
+    // adds the products not in decreasing profit percent
+
     public ArrayList<ArrayList<String>> readFromFile(String fileName) {
         try {
             ArrayList<ArrayList<String>> resultSet = new ArrayList<>();
             ArrayList<String> productInfo;
             Scanner file = new Scanner(new File(fileName));
             Gson gson = new Gson();
-            ArrayList<Double> profitPercent = new ArrayList<>();
-            Map<Double, ArrayList<String>> orderedProfit = new HashMap<>();
             while (file.hasNextLine()) {
                 productInfo = new ArrayList<>();
-                Products product = getProducts(productInfo, file, gson);
-                profitPercent.add(product.getProfitPercent());
-                orderedProfit.put(product.getProfitPercent(), productInfo);
-            }
-            Collections.sort(profitPercent);
-            for (int i = 0; i < profitPercent.size(); i++) {
-                resultSet.add(orderedProfit.get(profitPercent.get(profitPercent.size() - 1 - i)));
+                Products product = gson.fromJson(file.nextLine(), Products.class);
+                getProducts(productInfo, product);
+                resultSet.add(productInfo);
             }
             file.close();
             return resultSet;
@@ -50,8 +79,7 @@ public class FileWriter {
         return null;
     }
 
-    private Products getProducts(ArrayList<String> productInfo, Scanner file, Gson gson) {
-        Products product = gson.fromJson(file.nextLine(), Products.class);
+    private void getProducts(ArrayList<String> productInfo, Products product) {
         productInfo.add(product.getProductName());
         productInfo.add(String.valueOf(product.getRetailPrice()));
         productInfo.add(String.valueOf(product.getCostPrice()));
@@ -61,7 +89,6 @@ public class FileWriter {
         productInfo.add(String.valueOf(product.getTotalCost()));
         productInfo.add(String.valueOf(product.getProfit()));
         productInfo.add(String.format("%.2f", product.getProfitPercent()));
-        return product;
     }
 
     public void writeFile(String gson, String fileName) {
